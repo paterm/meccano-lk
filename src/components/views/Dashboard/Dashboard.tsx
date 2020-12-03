0ce6056bed9286621e56c702764943cba530d158
mukhindev <mukhin.dev@gmail.com>
2020-12-03 22:09:19 +0200

    Шапка главной страницы и кнопка-иконка

--- src/components/views/Dashboard/Dashboard.tsx
+++ src/components/views/Dashboard/Dashboard.tsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { classes } from '../../../utils/helpers';
+import Button from '../../ui/Button/Button';
+import { ReactComponent as AddIcon } from '../../../assets/icons/dashboard/add.svg';
+import { ReactComponent as ConfigIcon } from '../../../assets/icons/dashboard/config.svg';
+import './Dashboard.css';
+
+const cls = classes('dashboard');
 
 const Dashboard:React.FC = () => (
-  <div>
+  <div { ...cls() }>
     <Helmet>
       <title>Dashboard | Maeccano</title>
     </Helmet>
+    <div { ...cls('header') }>
+      <h2 { ...cls('title') }>
+        Михаил, добро пожаловать в Meccano!
+      </h2>
+      <div { ...cls('toolbar') }>
+        <Button
+          { ...cls('button') }
+          icon={ AddIcon }
+          color="gray"
+          rounded
+        />
+        <Button
+          { ...cls('button') }
+          icon={ ConfigIcon }
+          color="coral"
+          rounded
+        />
+      </div>
+    </div>
 
-    Dashboard
   </div>
 );
 
