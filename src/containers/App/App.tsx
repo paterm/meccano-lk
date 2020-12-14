da93bab26d2678151515b0397b59701514a8f84e
mukhindev <mukhin.dev@gmail.com>
2020-12-14 18:49:52 +0200

    Fix упростить конструкцию установки isMobile

--- src/containers/App/App.tsx
+++ src/containers/App/App.tsx
@@ -1,6 +1,7 @@
 import React, { Suspense, useEffect } from 'react';
 import { useHistory } from 'react-router';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { IStore } from '@interfaces';
 import { TProfile } from '@types';
 import Router from './Router';
 
@@ -13,29 +14,27 @@ import Sidebar from '../../components/layouts/Sidebar/Sidebar';
 
 const App: React.FC = () => {
   const dispatch = useDispatch();
+  const { isMobile } = useSelector((state: IStore) => state.mobile);
   const history = useHistory();
   const handleSignIn = () => history.push('/sign-in');
   const handleSignUp = () => history.push('/sign-up');
 
   useEffect(() => {
-    if (window.innerWidth < 768) dispatch(setMobile(true));
-    let isMobileWidth: boolean;
+    dispatch(setMobile(window.innerWidth < 768));
     const resizeListener = () => {
       if (window.innerWidth < 768) {
-        if (isMobileWidth === true) return;
-        isMobileWidth = !isMobileWidth;
-        dispatch(setMobile(true));
+        if (isMobile) return;
+        dispatch(setMobile(!isMobile));
       } else {
-        if (isMobileWidth === false) return;
-        isMobileWidth = !isMobileWidth;
-        dispatch(setMobile(false));
+        if (!isMobile) return;
+        dispatch(setMobile(!isMobile));
       }
     };
     window.addEventListener('resize', resizeListener);
     return () => {
       window.removeEventListener('resize', resizeListener);
     };
-  }, [ dispatch ]);
+  }, [ dispatch, isMobile ]);
 
   useEffect(() => {
     const User: TProfile = {
